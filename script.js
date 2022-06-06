7d22ffee091ffbfd82d5390be3b2e3a970dcf953
Adi <adib09132005@gmail.com>
2022-06-05 23:27:49 -0500

    idk messing around ig

--- script.js
+++ script.js
@@ -1,63 +1,119 @@
+//init scores for win logic later
+
 let wins = 0;
 let losses = 0;
 let draws = 0;
+
+
+
+// Logic to return random computer choice
 function computerPlay() {
-    let num = Math.floor((Math.random() * 3));
-    if (num == 2) {
+    let num = Math.floor((Math.random() * 3)) + 1;
+    if (num == 3) {
         return "rock";
-    } else if (num == 1) {
+    } else if (num == 2) {
         return "paper";
     } else {
         return "scissors";
     }
 }
 
+//play single round of game
 function playRound(playerSelection, computerSelection) {
-    let playerChoice = playerSelection.toLowerCase();
+    console.log(computerSelection);
+    if (playerSelection === computerSelection) draws++;
+    else if (
+        (playerSelection === "rock" && computerSelection === "paper")
+        || (playerSelection === "paper" && computerSelection === "scissors")
+        || (playerSelection === "scissors" && computerSelection === "rock")
+    ) {
+        losses++;
+    }
+    else {
+        wins++
+    }
+    updateScore();
+    console.log(`You have ${wins} wins, ${losses} losses, and ${draws} draws`);
+}
 
-    if (playerChoice === computerSelection) return "Draw!";
 
-    else if (playerChoice === "paper") {
-        if (computerSelection === "scissors") return "Player Loses! Computer Wins :(";
-        if (computerSelection === "rock") return "Player Wins! Computer Loses :";
-    } else if (playerChoice === "rock") {
-        if (computerSelection === "paper") return "Player Loses! Computer Wins :(";
-        if (computerSelection === "scissors") return "Player Wins! Computer Loses :";
-    } else if (playerChoice === "scissors") {
-        if (computerSelection === "rock") return;
-        if (computerSelection === "paper") return "Player Wins! Computer Loses :";
+//return true if game is over and false if not
+function isGameOver() {
+    return (wins === 5 || losses === 5) ? true : false;
+}
+
+
+//logic to keep playing rounds til game is over
+function game(playerSelection) {
+
+    if (isGameOver()) {
+        gameOver();
     } else {
-        return playRound(prompt("Error. You may have mispelled the word. Try again"), computerSelection);
+        let computerSelection = computerPlay();
+        playRound(playerSelection.toLowerCase(), computerSelection);
     }
+    if (isGameOver()) {
+        gameOver();
+    }
+
+
+
+}
+const scoreContainer = document.querySelector("#container");
+const score = document.createElement('div');
+
+function updateScore() {
+
+    score.classList.add("score");
+    score.innerHTML = `You: ${wins} Computer: ${losses}. Draws: ${draws}`;
+    scoreContainer.appendChild(score);
 
 }
 
-function game() {
-    let whileBool = true;
-
-    while (whileBool) {
-
-        const playerSelection = prompt("Rock, Paper, or Scissors?")
-        const computerSelection = computerPlay();
-        let result = playRound(playerSelection, computerSelection);
-        console.log(result)
-
-        if (result === "Draw!") draws++;
-        else if (result === "Player Wins! Computer Loses :") wins++;
-        else if (result === "Player Loses! Computer Wins :(") losses++;
-        console.log(`You have ${wins} wins, ${losses} losses, and ${draws} draws`);
-        if (losses == 5) {
-            console.log("You have lost the set of games");
-            whileBool = false;
-        } else if (wins == 5) {
-            console.log("Congrats! You have won the set of games!");
-            whileBool = false;
-        }
-    }
-
+function winOrLoss(){
+    if(score === 5) return "win";
+    else return "loss";
+    
 }
 
 
-game();
+//puts buttons away and then adds a "game over" splash
+function gameOver() {
+    const buttons = document.querySelectorAll(".btn");
+    buttons.forEach((button) => {
+        button.style.display = "none";
 
-console.log(`You had ${wins} wins, ${losses} losses, and ${draws} draws`);
+    });
+
+    if(winOrLoss === "win"){
+        const box = document.getElementById('box');
+        box.style.display = "block";
+    }
+
+
+
+}
+
+//init and add event listeners to buttons
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+
+rockButton.addEventListener('click', () => {
+    game("rock");
+});
+paperButton.addEventListener('click', () => {
+    game("paper");
+});
+scissorsButton.addEventListener('click', () => {
+    game("scissors");
+});
+
+
+
+
+
+
+
+
+
